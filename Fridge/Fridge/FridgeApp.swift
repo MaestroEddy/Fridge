5f3abcdc7d6a4715946197377248742deab93b4b
Edgar Borovik <e2.borovik@itransition.com>
2021-05-03 15:41:06 +0200

    initial

---
+++ Fridge/Fridge/FridgeApp.swift
@@ -0,0 +1,18 @@
+//
+//  FridgeApp.swift
+//  Fridge
+//
+//  Created by Borovik, Edgar2 on 3.05.21.
+//
+
+import SwiftUI
+
+@main
+struct FridgeApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
